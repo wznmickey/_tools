b2928b80c1eeeda0915ba20b4c630dec550e6aa0
wznmickey <first@wznmickey.com>
2025-01-29 17:12:36 -0500

    llm

--- _tools.py
+++ _tools.py
@@ -16,13 +16,13 @@ def clearLines(x):
 def translateHtml(x):
     import os
     temp = clearLines(x)
-    os.system("python -m googletranslate zh-CN " + '"' + x + '"')
+    os.system("/home/wznmickey/.pyenv/versions/3.10.13/bin/python -m googletranslate zh-CN " + '"' + x + '"')
 
 
 def translate(x):
     import os
     temp = clearLines(x)
-    os.system("python -m googletranslate -r text zh-CN " + '"' + x + '"')
+    os.system("/home/wznmickey/.pyenv/versions/3.10.13/bin/python -m googletranslate -r text zh-CN " + '"' + x + '"')
 
 def deeplx(x):
     import httpx, json
@@ -43,6 +43,19 @@ def deeplx(x):
     for i in json.loads(r.text)["alternatives"] or []:
         print(unquote(i))
 
+from ollama import chat
+from ollama import ChatResponse
+def llm(x):
+    response: ChatResponse = chat(model='qwen2.5:0.5b', messages=[
+    {
+        'role': 'user',
+        'content': 'Translate this to Chinese: '+x,
+    },
+    ])
+    print(response['message']['content'])
+    # or access fields directly from the response object
+    print(response.message.content)
+
 if __name__ == '__main__':
     argv = sys.argv
     if (argv[1] == "clearLines"):
@@ -54,3 +67,5 @@ if __name__ == '__main__':
         translate(argv[2])
     if (argv[1] == "deeplx"):
         deeplx(argv[2])
+    if (argv[1] == "llm"):
+        llm(argv[2])
